818607036a67709d9949a419cfad790c2a3dfd9b
Dhanush0369 <dhanush789245@gmail.com>
2025-07-28 19:01:14 +0530

    Add Data Schemas

---
+++ schema.ts
@@ -0,0 +1,31 @@
+export type TokenSchema = {
+    token_address: string;
+    token_name: string;
+    token_ticker: string;
+    price_usd: number;
+    market_cap_usd: number;
+    volume_1h: number;
+    volume_6h: number;
+    volume_24h: number;
+    liquidity_usd: number;
+    transaction_count_1h: number;
+    transaction_count_6h: number;
+    transaction_count_24h: number;
+    price_1hr_change: number;
+    price_6hr_change: number;
+    price_24hr_change: number;
+    protocol: string;
+};
+
+export type FilterDataSchema = {
+    token_address: string;
+    token_name: string;
+    token_ticker: string;
+    price_usd: number;
+    market_cap_usd: number;
+    volume: number;
+    liquidity_usd: number;
+    transaction_count: number;
+    price_change: number;
+    protocol: string;
+};
