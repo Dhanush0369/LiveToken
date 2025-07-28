ea37fb582ddb05aca97afd1b5955289746888250
Dhanush0369 <dhanush789245@gmail.com>
2025-07-28 18:59:19 +0530

    Add token & pool Address Data

---
+++ token_data.ts
@@ -0,0 +1,24 @@
+export const tokens: [string, string][]=[];
+
+// [token_address, pool_address]
+tokens.push(["LAYER4xPpTCb3QL8S9u41EAhAX7mhBn8Q6xMTwY2Yzc","G6drsaPCR3pxsEmSTAc81kW1EL3kFAFwtSAkzUZXmgH3"]);
+tokens.push(["SNS8DJbHc34nKySHVhLGMUUE72ho6igvJaxtq9T3cX3","25YwWosqxtHfam9xrBedgqSqtqZ6PLF2REnWWtRSTYRf"]);
+tokens.push(["EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v","4fuUiYxTQ6QCrdSq9ouBYcTM7bqSwYTSyLueGZLTy4T4"]);
+tokens.push(["mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So","8EzbUfvcRT1Q6RL462ekGkgqbxsPmwC5FMLQZhSPMjJ3"]);
+tokens.push(["AVLhahDcDQ4m4vHM4ug63oh7xc8Jtk49Dm5hoe9Sazqr","CR8FJB9jqGvtNYnYUuxyyS41WXue2HZRfhNavkaV8CS4"]);
+tokens.push(["7dHbWXmci3dT8UFYWYZweBLXgycu7Y3iL6trKn1Y7ARj","7EJSgV2pthhDfb4UiER9vzTqe2eojei9GEQAQnkqJ96e"]);
+tokens.push(["4MeqZ8v3XbNVVX6B69XxXaymoTX6DbK8iZgaGngCbonk","4MXybVn82rBxjvANiMpmvYUQejRzwkiTnjDT8NQmHMRe"]);
+tokens.push(["5NJfQ6UQ1LJTxtKLrsZQcbiSUWequSD6aZLNJaN7Zviv","6qyPDoLfSFKeZ1BXdP2GzCgUsh1BQTVQc42S9yQmbt78"]);
+tokens.push(["susdabGDNbhrnCa6ncrYo81u4s9GM8ecK2UwMyZiq4X","6GGYCNjZ5jqNFRfjJqCRdYiiTZY47LfweZXm4TyXzcm7"]);
+tokens.push(["SCSuPPNUSypLBsV4darsrYNg4ANPgaGhKhsA3GmMyjz","HWzE4bSqxXR3L2pCrdaCEFNJCQXQN4gjPWEikwTDjwq7"]);
+tokens.push(["E4FKNCodGw5PM35mMA5jvKB3pUibaxFwSskCiCmgczkD","FCogRPKDGKxgnCqt8VSeRxj3fk5kc9tfUj8F1DM1YuhQ"]);
+tokens.push(["Hbsyh8faSA4zJKNkTQmT6XQj8oDDdA2tvgA2yE7WBeEG","5KAz8pY97ostVFEfduag5hAxjrUHfpF21mufm5XBEhQN"]);
+tokens.push(["GzH4iMB2gbeteRoW3RyGV9xR1yYuu56mA2inxH65u99P","53EQhjXrnVpURhXBL7ubAxYhjPXeG8FzW3i7kynt7DN"]);
+tokens.push(["7P5pMrzZBgCM8hT2B6fieTP7XKqNeKPyYVfZnbo44y8X","B6pv8RAxJkTU6XACjcWjH3Pwf14V5NbdeCwC1by5ZgbR"]);
+tokens.push(["2f5o9crg1nxP2gPZCbVsTjRAgPnHq8cHrFyALvTGq6vC","8Pw9fHSMwJDzZmsdtvBFu1mfq18C5B8upvZy5rS7GLCC"]);
+tokens.push(["Cpnm1fZUjm48M54Gaae2kwQfMgHX9NZHozhyVb55rfyD","EbYMiGkh36mcQ6sHDdizW96Qd3CsM9GTw8kSbA4fPvhv"]);
+tokens.push(["CkPSkb4EmWGbWMKH8RFS2DZj1UAphfJeAyhEys9VPiM3","BJj6ujRepfGBHCwNg2W6DFd27KwjULnWrcEqz9ggQNgS"]);
+tokens.push(["DeaKMzAeZja3Mh5okZE6WUvygLP3Lfuvm6Rg78HqXTz9","GqpCwsBbmcCPwwPaJevdsfK9M5zeAoDBd96yxHUpdgrW"]);
+tokens.push(["F23fFqpRNsmWjuUrFpfM1pvoVvMSpLuN6hY978Y1JXLt","EJQxe3iyrGdFteR3vU6ZDaiy4xvB2Aj9AQCFhLCjNtJH"]);
+tokens.push(["J1toso1uCk3RLmjorhTtrVwY9HJ7X8V9yYac6Y7kGCPn","5hWJUNTtEtKmKgDXpthJXXRRmJrz5vJ7uJzrUNVdrwLg"]);
+
