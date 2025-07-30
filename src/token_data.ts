63585f744f3f74b9c3ec76ac7d01c65bedb30316
Dhanush0369 <dhanush789245@gmail.com>
2025-07-30 12:55:58 +0530

    Update token address Data

--- src/token_data.ts
+++ src/token_data.ts
@@ -13,14 +13,14 @@ tokens.push(["AVLhahDcDQ4m4vHM4ug63oh7xc8Jtk49Dm5hoe9Sazqr","CR8FJB9jqGvtNYnYUux
 tokens.push(["4MeqZ8v3XbNVVX6B69XxXaymoTX6DbK8iZgaGngCbonk","4MXybVn82rBxjvANiMpmvYUQejRzwkiTnjDT8NQmHMRe"]);
 tokens.push(["5NJfQ6UQ1LJTxtKLrsZQcbiSUWequSD6aZLNJaN7Zviv","6qyPDoLfSFKeZ1BXdP2GzCgUsh1BQTVQc42S9yQmbt78"]);
 tokens.push(["SCSuPPNUSypLBsV4darsrYNg4ANPgaGhKhsA3GmMyjz","HWzE4bSqxXR3L2pCrdaCEFNJCQXQN4gjPWEikwTDjwq7"]);
-tokens.push(["E4FKNCodGw5PM35mMA5jvKB3pUibaxFwSskCiCmgczkD","FCogRPKDGKxgnCqt8VSeRxj3fk5kc9tfUj8F1DM1YuhQ"]);
-// tokens.push(["Hbsyh8faSA4zJKNkTQmT6XQj8oDDdA2tvgA2yE7WBeEG","5KAz8pY97ostVFEfduag5hAxjrUHfpF21mufm5XBEhQN"]);
-tokens.push(["GzH4iMB2gbeteRoW3RyGV9xR1yYuu56mA2inxH65u99P","53EQhjXrnVpURhXBL7ubAxYhjPXeG8FzW3i7kynt7DN"]);
 tokens.push(["7P5pMrzZBgCM8hT2B6fieTP7XKqNeKPyYVfZnbo44y8X","B6pv8RAxJkTU6XACjcWjH3Pwf14V5NbdeCwC1by5ZgbR"]);
-// tokens.push(["2f5o9crg1nxP2gPZCbVsTjRAgPnHq8cHrFyALvTGq6vC","8Pw9fHSMwJDzZmsdtvBFu1mfq18C5B8upvZy5rS7GLCC"]);
 tokens.push(["Cpnm1fZUjm48M54Gaae2kwQfMgHX9NZHozhyVb55rfyD","EbYMiGkh36mcQ6sHDdizW96Qd3CsM9GTw8kSbA4fPvhv"]);
 tokens.push(["CkPSkb4EmWGbWMKH8RFS2DZj1UAphfJeAyhEys9VPiM3","BJj6ujRepfGBHCwNg2W6DFd27KwjULnWrcEqz9ggQNgS"]);
 tokens.push(["DeaKMzAeZja3Mh5okZE6WUvygLP3Lfuvm6Rg78HqXTz9","GqpCwsBbmcCPwwPaJevdsfK9M5zeAoDBd96yxHUpdgrW"]);
 tokens.push(["F23fFqpRNsmWjuUrFpfM1pvoVvMSpLuN6hY978Y1JXLt","EJQxe3iyrGdFteR3vU6ZDaiy4xvB2Aj9AQCFhLCjNtJH"]);
+//tokens.push(["E4FKNCodGw5PM35mMA5jvKB3pUibaxFwSskCiCmgczkD","FCogRPKDGKxgnCqt8VSeRxj3fk5kc9tfUj8F1DM1YuhQ"]);
+// tokens.push(["Hbsyh8faSA4zJKNkTQmT6XQj8oDDdA2tvgA2yE7WBeEG","5KAz8pY97ostVFEfduag5hAxjrUHfpF21mufm5XBEhQN"]);
+//tokens.push(["GzH4iMB2gbeteRoW3RyGV9xR1yYuu56mA2inxH65u99P","53EQhjXrnVpURhXBL7ubAxYhjPXeG8FzW3i7kynt7DN"]);
+// tokens.push(["2f5o9crg1nxP2gPZCbVsTjRAgPnHq8cHrFyALvTGq6vC","8Pw9fHSMwJDzZmsdtvBFu1mfq18C5B8upvZy5rS7GLCC"]);
 
 
